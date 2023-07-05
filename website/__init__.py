1def5242b6d3dd33b53adff1f384189dc0050b80
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2023-07-05 12:27:14 +0300

    creating the user model

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
@@ -17,6 +18,8 @@ def create_app():
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
 
     create_database(app)
 
