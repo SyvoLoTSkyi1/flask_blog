e5258b1218c0673cbeb11a1529327ba5fcbc5a6a
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2023-07-08 19:51:02 +0300

    post database model

--- website/models.py
+++ website/models.py
@@ -9,4 +9,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passaive_deletes=True)
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Model(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
